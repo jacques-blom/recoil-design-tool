72fcda277f1f1810ae3d6e5ffd601dbe86f9916f
Jacques Blom <39128232+jacquesnf@users.noreply.github.com>
2020-07-20 20:22:21 +0200

    Image details

---
+++ functions/image-details.js
@@ -0,0 +1,56 @@
+const axios = require('axios').default
+const AWS = require('aws-sdk')
+
+const dynamodbClient = new AWS.DynamoDB({
+    region: 'us-east-1',
+    accessKeyId: process.env.JB_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.JB_AWS_SECRET_ACCESS_KEY,
+})
+
+const run = async (seed) => {
+    const cachedImage = await dynamodbClient
+        .getItem({
+            TableName: 'recoil-design-tool-images',
+            Key: {id: {S: seed}},
+        })
+        .promise()
+
+    if (!cachedImage.Item) {
+        throw new Error('Not found')
+    }
+
+    const location = cachedImage.Item.location.S
+    const id = location.split('id/')[1].split('/')[0]
+
+    const {data} = await axios.get(`https://picsum.photos/id/${id}/info`)
+
+    return data
+}
+
+// The Netlify Functions handler function
+exports.handler = async function (event) {
+    try {
+        const body = await run(event.queryStringParameters.seed)
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(body),
+        }
+    } catch (error) {
+        if (error.message === 'Not found') {
+            return {
+                statusCode: 404,
+                body: error.message,
+            }
+        }
+
+        return {
+            statusCode: 500,
+            body: 'Internal Server Error',
+        }
+    }
+}
+
+if (process.env.LOCAL_TEST) {
+    run(process.env.SEED).then(console.log).catch(console.log)
+}

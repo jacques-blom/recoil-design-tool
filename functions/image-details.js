84d5caba08c552a13c10e1271d9aed8e29121f74
Jacques Blom <39128232+jacquesnf@users.noreply.github.com>
2020-07-20 20:38:54 +0200

    Content type

--- functions/image-details.js
+++ functions/image-details.js
@@ -35,6 +35,7 @@ exports.handler = async function (event) {
         return {
             statusCode: 200,
             body: JSON.stringify(body),
+            headers: {'Content-Type': 'application/json'},
         }
     } catch (error) {
         if (error.message === 'Not found') {

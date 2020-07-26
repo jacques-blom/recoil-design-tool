11b3e100df22108e8001f4aa80a4d28628c22f2c
Jacques Blom <39128232+jacquesnf@users.noreply.github.com>
2020-07-26 22:05:53 +0200

    Padding

--- src/Banner.tsx
+++ src/Banner.tsx
@@ -7,11 +7,11 @@ const Container = styled.div`
     align-items: center;
     justify-content: center;
     flex-direction: column;
-    height: 100px;
     font-size: 20px;
     font-weight: 600;
     line-height: 1.5;
     background-color: #101010;
+    padding: 20px;
 
     a {
         color: #da3084;
